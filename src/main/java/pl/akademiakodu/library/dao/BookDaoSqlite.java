48eaa03a463a95578452c097c8030b34df872b2f
Damian Mielcarekan <damian.mielcarek@gmail.com>
2016-11-17 11:22:30 +0100

    added reflection to BookDaoSqlite

--- src/main/java/pl/akademiakodu/library/dao/BookDaoSqlite.java
+++ src/main/java/pl/akademiakodu/library/dao/BookDaoSqlite.java
@@ -2,6 +2,7 @@ package pl.akademiakodu.library.dao;
 
 import pl.akademiakodu.library.domain.Book;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -51,9 +52,32 @@ public class BookDaoSqlite implements pl.akademiakodu.library.dao.BookDao {
 
     @Override
     public void addBook(Book book) {
+        /* 1
         String sql = "INSERT INTO Books (title, author, pages) "
                 +"VALUES ('"+book.getTitle()+"',' "+book.getAuthor()+"', "+book.getPages()+")";
+        */
+        // refleksja = dzięki refleksji jest to możliwe
+        String simpleClassName = book.getClass().getSimpleName();
+        System.out.println(simpleClassName);
+        Field[] fields = book.getClass().getDeclaredFields();
+        StringBuilder attributeString = new StringBuilder("(");
 
+        for (Field field : fields) {
+            if ( !field.getName().equals("id")){
+                attributeString.append(field.getName());
+                attributeString.append(",");
+            }
+            System.out.println(field.getName());
+            System.out.println(field.getType());
+        }
+        attributeString.deleteCharAt(attributeString.length()-1);
+        attributeString.append(")");
+        System.out.println(attributeString);
+
+
+
+        String sql = "INSERT INTO "+simpleClassName+"s "+attributeString
+                +"VALUES ('"+book.getTitle()+"',' "+book.getAuthor()+"', "+book.getPages()+")";
         try{
             Statement statement = connection.createStatement();
             statement.execute(sql);
